bb396b552edfb1fd9db81ef7aecaf2fde6395126
Mardus <24569313@student.g.nwu.ac.za>
2018-02-13 15:54:13 +0200

    Interface
    Interface with textboxes

---
+++ app/src/main/java/com/example/jedidja/quest/CreationFormActivity.java
@@ -0,0 +1,13 @@
+package com.example.jedidja.quest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreationFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation_form);
+    }
+}
